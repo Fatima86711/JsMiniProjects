b17a9cd06f37a325f6f5f108690ee14696a02106
Fatima86711 <fatimazafar411133@gmail.com>
2025-01-19 22:04:35 +0500

    Add files via upload

--- countDown/app.js
+++ countDown/app.js
@@ -1,16 +1,21 @@
-const endDate = "18 January 2025 3:00 AM";
+const endDate = "20 August 2025 12:00 AM";
 document.getElementById("end-date").innerText = endDate;
 const inputs = document.querySelectorAll("input");
 function clock() {
   const then = new Date(endDate);
   const now = new Date();
   diff = Math.floor((then - now) / 1000);
-  inputs[0].value = Math.floor(diff / 3600 / 24);
-  inputs[1].value = Math.floor((diff / 3600) % 24);
-  inputs[2].value = Math.floor((diff / 60) % 60);
-  inputs[3].value = Math.floor(diff % 24);
+  if (diff >= 0) {
+    inputs[0].value = Math.floor(diff / 3600 / 24);
+    inputs[1].value = Math.floor((diff / 3600) % 24);
+    inputs[2].value = Math.floor((diff / 60) % 60);
+    inputs[3].value = Math.floor(diff % 60);
+  } else {
+    console.log("ENDED");
+  }
 }
-clock();
 setInterval(() => {
   clock();
 }, 1000);
+
+clock();
